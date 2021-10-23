cb5ad1b55edb42bec52941bd439427d1fe5475b8
jot6000 <joemercer50@googlemail.com>
2021-10-23 15:07:52 +0100

    Created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      inputValue:''   //Make a key value pair in state for each input field you want to controll
+    }
+  }
+
+  //Create a function for each input field to update its assosiated state
+  //e is passed as an argument so that we have access to the 'event'
+  textareaChange=(e)=>{
+    this.setState({inputValue:e.target.value})
+  }
+  
+  formSubmit=()=>{
+
+    /*
+      Do what we want with form data here
+    */
+
+    //Clear input fields like this(you can sperate the key value pairs with commas if you need to clear more than one)
+    this.setState({inputValue:''})
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>
+          Controlled form example
+        </h1>
+        <form onSubmit={this.formSubmit /* we  set the on submit function for the form*/}>
+          <textarea 
+            value={this.state.inputValue /*We set the value of the field to our state, this occurs after onChange fires so it takes the new state */} 
+            onChange={(e)=>this.textareaChange(e) /*we set up an onChange event that fires off our function and updates the state before the value is updated*/}
+          />
+          <input type='submit'/>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
